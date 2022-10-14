d08ebbc4f20101fb26811f270ec6811d4b1b1120
guillermo rodriguez <guillerhuguet@gmail.com>
2022-10-14 18:11:58 +0100

    apuntes cliente

---
+++ ejerciciosCliente/script/menu.js
@@ -0,0 +1,15 @@
+window.alert("Introduce un numero del 1-5");
+
+var a = prompt("Selecciona");
+switch(a){
+    case "1": document.write("Has seleccionado 1");
+    break;
+    case "2": document.write("Has seleccionado 2");
+    break;
+    case "3": document.write("Has seleccionado 3");
+    break;
+    case "4": document.write("Has seleccionado 4");
+    break;
+    case "5": document.write("Has seleccionado 5");
+    break;
+}
---
+++ ejerciciosCliente/script/parimpar.js
@@ -0,0 +1,9 @@
+
+for(var i=1;i<=50;i++){
+    document.write(i);
+    if(i%2==0){
+        document.write(": es par<br> ");
+    }else{
+        document.write(": es impar<br>");
+    }
+}
