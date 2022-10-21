ecbea67b989047e0923d87d6c17f07e1eff841df
guillermo rodriguez <guillerhuguet@gmail.com>
2022-10-21 19:25:19 +0100

    fin clase 21-10

--- data/ejerciciosCliente/script/dom.js
+++ data/ejerciciosCliente/script/dom.js
@@ -1,19 +1,19 @@
-function paginaCargada(){
+function paginaCargada() {
     debugger;
     var elemento = document.getElementById("div1");
     var elementoClases = document.getElementsByClassName("claseParrafo");
-    console.log("Elemento:"+elemento);
-    console.log("Clase parrafo: "+elementoClases);
+    console.log("Elemento:" + elemento);
+    console.log("Clase parrafo: " + elementoClases);
 }
 
 
 
 //Genera parrafo de forma dinamica
-function generarParrafo(){
+function generarParrafo() {
     //Generamos parrafo y contenido
-    var parrafo=document.createElement("p");
+    var parrafo = document.createElement("p");
     //Indicamos id del parrafo creado
-    parrafo.id="p3";
+    parrafo.id = "p3";
     var nodo = document.createTextNode("Parrafo dinamico");
     //Añadimos el nodo al nuevo parrafo
     parrafo.appendChild(nodo);
@@ -22,24 +22,62 @@ function generarParrafo(){
     element.appendChild(parrafo);
 }
 
-function eliminarParrafo(){
+function eliminarParrafo() {
     var element = document.getElementById("div1");
-   // var element = document.getElementsByClassName("claseParrafo");
+    // var element = document.getElementsByClassName("claseParrafo");
 
     element.removeChild(element.lastChild);
-   // element.parentNode.removeChild(element);
+    // element.parentNode.removeChild(element);
 
 }
 
 
-function modificarParrafo(){
-    var element = document.getElementById("div1");
+function modificarParrafo() {
+    //var element = document.getElementById("p3").innerHTML;
     var valor = prompt("¿Que valor quieres asignarles?");
     //element.innerHTML(element.childNodes)=valor;
 
-    element.replaceChildren(valor);
-  
+    // element.replaceChildren(valor);
+    document.getElementById("p3").innerHTML = valor;
+
 }
 //Generar un boton con funcion eliminarParrafo() que elimine el parrafo con id="p3" generado previamente
 //Para ello generaremos igualmente una variable con el parrafo a eliminar
 //y usaremos la instruccion variable.remove 
+
+
+/*
+- Generar una variable que contenga todos los hijos de div1
+para ello usar la propiedad childNodes
+usar ademas los atributos firstchild y last child para ver sus hijos
+
+-Generar la lista desordenada en html y usar las propiedades nextSibling y previousSibling 
+para moverse entre nodos hermanos
+
+<ul>
+<li id="item1">elemento primero</li><li id="item2">elemento2</li>
+</ul>
+
+- que pasa si tengo ospacios o mas elementos entre los elementos de tipo "li"?
+
+*/
+
+function hijosdiv1() {
+    debugger;
+    var hijosdiv = document.getElementById("div1");
+    var hijos = document.getElementById("div1").childNodes;
+
+    var longintud = document.getElementById("div1").childNodes.length;
+
+   /* document.write(hijosdiv.nextSibling);
+    var longintud = document.getElementById("div1").childNodes.length;
+    for(var i = 0; i< longintud;i++){
+        document.write(hijosdiv.childNodes[i].innerHTML+"<br>");
+    }
+    document.write(hijosdiv);*/
+   // document.write(hijos[1].textContent.);
+
+    while(hijos.nextSibling){
+    document.write(hijos[1].nextSibling.textContent);
+    }
+}
---
+++ data/ejerciciosCliente/script/dom2.js
@@ -0,0 +1,41 @@
+function main(){
+
+}
+function navegarNodos(){
+    debugger;
+    var nodosHijos = document.getElementById("div1").childNodes;
+    console.log(nodosHijos.length);
+    console.log(nodosHijos[1]);
+    console.log(nodosHijos[1].textContent);
+
+
+    //parentNode
+    var nodosPadre = document.getElementById("p1").parentNode;
+    //Preguntar: porque esta devolviendo Undefined
+    console.log(nodosPadre[0]);
+
+    //firstChild
+    var primerHijo = document.getElementById("div1").firstChild;
+    console.log(primerHijo);
+
+    //lastChild
+    var ultimohijo = document.getElementById("div1").lastChild;
+    console.log(ultimohijo);
+
+
+    var textoSiguienteHermano = document.getElementById("item1").nextSibling.innerHtml;
+
+    //previousSibling
+
+    var item2 = document.getElementById("item2");
+    var textoAnteriorHermano = document.getElementById("item2").previousSibling.innerHtml;
+    console.log(textoAnteriorHermano);
+
+    //Atributo nodeName
+    console.log(nodosHijos[0].nodeName);
+    console.log(nodosHijos[3].nodeName);
+
+    //Para recorrer un padre y comprobar si los elementos son validos, puedo usar las siguientes 
+    //condiciones
+    //nodosHijos[3].tagName==null;
+}
