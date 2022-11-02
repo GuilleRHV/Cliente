2be54f91a15febb4c49d85d56be50429ac12b422
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-02 18:46:56 +0000

    asa

--- data/ejerciciosCliente/tablasdinamicas/scripts.js
+++ data/ejerciciosCliente/tablasdinamicas/scripts.js
@@ -3,23 +3,28 @@ function main() {
 
     var filas = ['azul', 'rojo', 'verde', 'amarillo', 'morado'];
     var columnas = ['triángulo', 'cuadrado', 'pentágono', 'heptágono', 'hexágono'];
-
-    document.createElement("table");
+    var elemento = document.createElement("h3");
+    elemento.textContent = "prueba";
+    document.write(elemento.innerHTML);
+    debugger;
+    var tabla = document.createElement("table border=2");
     for (var fil = 0; fil < filas.length; fil++) {
 
         var hilera = document.createElement("tr");
-        
+        document.write(hilera.innerHTML);
         for (var col = 0; col < columnas.length; col++) {
             var celda = document.createElement("td");
+            document.write(celda.innerHTML);
             var textocelda = document.createTextNode(columnas[col]);
             celda.appendChild(textocelda);
             hilera.appendChild(celda);
         }
-        debugger;
-     table.appendChild(hilera);   
+
+        table.appendChild(hilera);
     }
-    table.setAttribute("border","2");
+    document.write(tabla.innerHTML);
+    tabla.setAttribute("border", "2");
     document.write(table);
-    
-    
+
+
 }
