63e04571e9ca813c63ce1892f881e14418ff96da
guillermo rodriguez <guillerhuguet@gmail.com>
2022-11-23 19:24:35 +0000

    cli

--- data/ejerciciosCliente/tema2/script/eventos.js
+++ data/ejerciciosCliente/tema2/script/eventos.js
@@ -3,6 +3,10 @@ function cambiarParrafoColor(){
     p3.style.color ="red";
 }
 //Cambios en tiempo de ejecucion
+
+function btnAtarEvento_Click(){
+atarEvento();
+}
 function atarEvento(){
     alert("Se ha atado el evento onmouseover al parrafo p3\n pasa el cursor sobre el parrafo");
     document.getElementById("p3").onmouseover = cambiarParrafoColor;
@@ -17,3 +21,25 @@ function dispararEventoDiv(event){
     alert("Div: "+event);
 
 }
+
+
+/**////////////////////////////////////////////// */
+function cambiarParrafoColorEjercicio(){
+    parrafo = document.getElementById("p4");
+    p4.style.color ="blue";
+}
+function cambiarTextoEjercicio(){
+    parrafo = document.getElementById("p4");
+    p4.innerHTML = "Texto cambiado";
+    p4.style.color ="blue";
+}
+function btnEnlazarEjercicio(){
+ //   document.getElementById("p4").addEventListener("onmouseover",cambiarTextoEjercicio);
+    document.getElementById("p4").onmouseover=cambiarTextoEjercicio;
+}
+function btnDesenlazarEjercicio(){
+    document.getElementById("p4").removeEventListener("onmouseover",cambiarTextoEjercicio);
+}
+//Generar un boton  k permita enlazar un parrago 2 eventos mouseover y mouseout 
+//de forma que el contenido del parrafo cambie cuando se pase el cursor sobre el parrafo
+//generar ademas un 2do boton el codigo para desenlazar dichos elementos
