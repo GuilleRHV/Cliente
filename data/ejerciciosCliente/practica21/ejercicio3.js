91689e16f386da5814687432cf234de25a70bfe9
guille <guillerhuguet@gmail.com>
2022-12-05 13:30:55 +0000

    ej4

--- data/ejerciciosCliente/practica21/ejercicio3.js
+++ data/ejerciciosCliente/practica21/ejercicio3.js
@@ -2,66 +2,97 @@
 
 function main() {
     
-    contenedor = document.getElementById("contPrincipal");
-    espacio = document.createElement("div");
-    
-    semana = ["lunes","martes","miercoles","jueves","viernes","sabado"];
-    for (var h = 0; h <6; h++) {
-       var dia = document.createElement("h3");
-        dia.innerHTML=semana[h];
-       contenedor.appendChild(dia);
-        crearTabla();
 
+    
+    
+
+//Actividades
+
+    var deportes = ["pilates","pesas","spinning","estiramientos","boxeo","yoga","descanso","meditacion"];
+
+    for (var i=0;i<6;i++){
+        deportes.forEach((dep,i) => {
+            var td1 = document.createElement("td");
+            var txt1 = document.createTextNode(dep);
+            //No me sirve en este caso el mentodo crearFila
+            td1.appendChild(txt1);
+            var identif1 = (i+1).toString();
+            var mostrar1 = document.getElementById("ac"+identif1);
+            mostrar1.appendChild(td1);
+        });
+    }
+
+
+
+    
+//FISIO
+    var fisio = ["Andrea","Jose","Pedro","Laura","Andrea","Jose","Ivan","Ines","Pepe","Andrea"];
+
+    for (var i=0;i<5;i++){
+        fisio.forEach((persona,ind) => {
+            var td = document.createElement("td");
+            var txt = document.createTextNode(persona);
+            //No me sirve en este caso el mentodo crearFila
+            td.appendChild(txt);
+            var identif = (ind+1).toString();
+            var mostrar = document.getElementById("fi"+identif);
+            mostrar.appendChild(td);
+        });
     }
     
 
 
 }
 
-function crearTabla() {
 
-    ar8=["pilates","pesas","spinning","estiramientos","boxeo","yoga","descanso","meditacion"];
-    var cont=0;
+  function enviarfisio(){
+      var mostrar="";
+      var tablafisio=document.getElementById("tablafisio");
+      var persona = document.getElementsByName("fisio");
+      
+      var td = tablafisio.getElementsByTagName("td");
+      for (var i=0;i<persona.length;i++){
+          if(persona[i].checked == true){
+              mostrar = persona[i].value;
+              alert(persona[i].value);
+          }
+      }
+      
+    //  window.alert(td.length+"a");
+      //Ahora pintamos
+      for (var x = 0; x<5;x++){
+            for(var y=0; y<td.length;y++){
+               if(td[y].innerHTML==mostrar){
+                td[y].style.backgroundColor="yellow";
+                
+               }
+            }
+      }
+
+  }
+
+  function enviaractividad(){
+    var mostrar="";
+    var tablaact=document.getElementById("tablaact");
+    var actividad = document.getElementsByName("act");
     
-    contenedor = document.getElementById("contPrincipal");
-    
-    tabla = document.createElement("table");
-    tabla.border = 1;
-    tabla.id = "tabla";
-    for (var h = 7; h <= 21; h++) {
-
-        if (h % 2 != 0) {
-            var tr = document.createElement("tr");
-            var dato1=h+":00-"+(h+2)+":00";
-           var dato2=ar8[cont];
-            
-         
-            var td1 = document.createElement("td");
-            td1.innerHTML=dato1;
-            tr.appendChild(td1)
-
-            var td2 = document.createElement("td");
-             td2.innerHTML=dato2;
-        
-           tr.appendChild(td2);
-            tabla.appendChild(tr);
-            cont++;
+    var td = tablaact.getElementsByTagName("td");
+    for (var i=0;i<actividad.length;i++){
+        if(actividad[i].checked == true){
+            mostrar = actividad[i].value;
+            alert(actividad[i].value);
         }
     }
-    contenedor.appendChild(tabla);
-
+    
+  //  window.alert(td.length+"a");
+    //Ahora pintamos
+    for (var x = 0; x<5;x++){
+          for(var y=0; y<td.length;y++){
+             if(td[y].innerHTML==mostrar){
+              td[y].style.backgroundColor="yellow";
+              
+             }
+          }
+    }
 
 }
-
-
-    function crearColumna() {
-        var columna = document.createElement("tr");
-        return columna;
-    }
-    
-    function crearFila(contenido) {
-        var fila = document.createElement("td");
-        fila.innerHTML(contenido);
-        return fila;
-    }
-
