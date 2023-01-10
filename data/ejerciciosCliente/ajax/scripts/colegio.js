d77c797edb2ebd4e23af1e8f50e9785e61acedb5
guillermo rodriguez <guillerhuguet@gmail.com>
2023-01-10 17:49:32 +0000

    avanzando colegio2

--- data/ejerciciosCliente/ajax/scripts/colegio.js
+++ data/ejerciciosCliente/ajax/scripts/colegio.js
@@ -1,21 +1,56 @@
+class Colegio{
+    nombre="";
+    direccion="";
+    localidad="";
+    numeroAlumnos="";
+    numeroAulas="";
+    constructor(nombre,direccion,localidad,numeroAlumnos,numeroAulas){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.localidad = localidad;
+        this.numeroAlumnos = numeroAlumnos;
+        this.numeroAulas = numeroAulas;
+    }
+     toString(){
+        return "Nombre: " + this.nombre + ", direccion: " + this.direccion+ ", localidad: " + this.localidad + ", numeroAlumnos: " +this.numeroAlumnos+", numeroAulas: " +this.numeroAulas+"<br>";
+    }
+}
+
+
 function lanzarPeticionAsincrona(){
     //Generamos objeto
         var xhttp = new XMLHttpRequest();
+         colegios=[];
         //Asignamos funcion al evento readystate
         xhttp.onreadystatechange = function(){
             if(this.readyState == 4 && this.status==200){
                 document.getElementById("contenedor").innerHTML = this.responseText;
                 var varxml = this.responseXML;
                 var colegio = varxml.getElementsByTagName("colegio");
-                var arrayColegios[];
+                
                 for(var i=0; i<colegio.length; i++){
                     var nombre = varxml.getElementsByTagName("nombre")[i].innerHTML;
+                    var direccion = varxml.getElementsByTagName("direccion")[i].innerHTML;
+                    var localidad = varxml.getElementsByTagName("localidad")[i].innerHTML;
+                    var numeroAlumnos = varxml.getElementsByTagName("numeroAlumnos")[i].innerHTML;
+                    var numeroAulas = varxml.getElementsByTagName("numeroAulas")[i].innerHTML;
+
+                    var cole = new Colegio(nombre, direccion, localidad, numeroAlumnos, numeroAulas);
+                    colegios.push(cole);
+
                     console.log(nombre);
-                    arrayColegios[i]=varxml.getElementsByTagName("nombre")[i];
+            
                 }
                 
                 console.log("Hay "+colegio.length+ " colegios");
                 console.log("nombre: "+nombre);
+
+
+
+             document.write("<h2>Clase Colegio</h2>");
+             for(var i=0; i<colegios.length; i++){
+                document.write(colegios[i].toString());
+             }
             }
         };
     
