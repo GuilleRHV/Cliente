ce08d3e5a8b3f98dc85f143ad11c305c9ebd984d
guillermo rodriguez <guillerhuguet@gmail.com>
2023-01-27 18:31:40 +0000

    cliente botones

--- data/ejerciciosCliente/jquery/scripts/index.js
+++ data/ejerciciosCliente/jquery/scripts/index.js
@@ -10,14 +10,41 @@ $("body",function()
 {
 alert('alertt')
 }).append("<p>Pruebaaa</p>")*/
-
+$
 
 $("body",callBackDeBody()).append("<span>Text desde jquery</span> ")
+
+
+
+
+$("#30pxtitulo").click(function(){
+    $("#titulo").css("font-size","30px").css("font-weight","bold").css("color","red");
+});
+$("#quitar30pxtitulo").click(function(){
+    $("#titulo").css("font-size","").css("font-weight","").css("color","");
+});
+
+
+$("#24px").click(function(){
+    $("#tituloparrafo").css("font-size","24px").css("font-family","italic").css("color","diference").css("color","blue");
+});
+$("#quitar24px").click(function(){
+    $("#tituloparrafo").css("font-size","").css("font-family","").css("color","").css("color","");
+});
+
+$("#estilotexto").click(function(){
+    $(".info").css("font-size","20px").css("color","green");
+});
+$("#quitarestilotexto").click(function(){
+    $(".info").css("font-size","").css("color","");
+});
+
+
 
 
 });
 
 
 function callBackDeBody(){
-    alert("Se ha añadido un span (callbackdebody)")
+    //alert("Se ha añadido un span (callbackdebody)")
 }
